b751baa7cc6a8cb793ed49c1b9883f98f47e26fe
Daniel St. Vincent <59455549+danieljstvincent@users.noreply.github.com>
2020-10-23 22:45:29 -0700

    # Parstagram - Part I
    This is an Instagram clone with a custom Parse backend that allows a user to post photos and view a global photos feed.
    
    Time spent: **25** hours spent in total
    
    ## User Stories
    
    The following **required** functionality is completed:
    
    - [ X] User sees app icon in home screen and styled launch screen. (1pt)
    - [ X] User can sign up to create a new account. (1pt)
    - [X ] User can log in. (1pt)
    - [X ] User can take a photo, add a caption, and post it to the server. (3pt)
    - [ ] User can view the last 20 posts. (4pts)
    
    The following **bonus** features are implemented:
    
    - [ ] User can pull to refresh. (1pt)
    - [ ] User can load past tweets infinitely. (2pts)
    
    ## Video Walkthrough
    
    Here's a walkthrough of implemented user stories:
    
    <img src='http://g.recordit.co/10rxSu30B1.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />

--- InstagramMasterClone/FeedViewController.swift
+++ InstagramMasterClone/FeedViewController.swift
@@ -7,17 +7,59 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("owner")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error)in
+            if posts !=  nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+                
+            }
+        }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")
+        as! PostCell
+    
+        let post = posts[indexPath.row]
+        
+        let user = post["owner"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.captionLabel.text = post["caption"] as! String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL (string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        return cell
+    }
     }
 
     // MARK: - Table view data source
@@ -80,4 +122,4 @@ class FeedViewController: UIViewController {
     }
     */
 
-}
+
