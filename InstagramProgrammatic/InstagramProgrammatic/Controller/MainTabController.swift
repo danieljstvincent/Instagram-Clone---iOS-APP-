76cdf7cce77add18cecea0d27771833d9eabe09a
Daniel <dannystvincent@gmail.com>
2021-09-23 10:28:01 -0400

    Just Removed the storyboard and getting ready to diver in programmatically

---
+++ InstagramProgrammatic/InstagramProgrammatic/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramProgrammatic
+//
+//  Created by Daniel St Vincent on 9/23/21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func  viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
---
+++ InstagramProgrammatic/InstagramProgrammatic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramProgrammatic
+//
+//  Created by Daniel St Vincent on 9/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
