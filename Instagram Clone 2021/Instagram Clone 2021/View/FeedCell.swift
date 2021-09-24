bf08e9938722f94b2fe17565aedfb4af2213ea77
Daniel <dannystvincent@gmail.com>
2021-09-24 15:19:18 -0400

    profileImageView.layer.cornerRadius Implemented to make images round

--- Instagram Clone 2021/Instagram Clone 2021/View/FeedCell.swift
+++ Instagram Clone 2021/Instagram Clone 2021/View/FeedCell.swift
@@ -7,4 +7,32 @@
 
 import UIKit
 
-
+class FeedCell: UICollectionViewCell {
+    // MARK: - Properties
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        return iv
+    } ()
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
+        profileImageView.setDimensions(height: 40, width: 40)
+// Below is the line of code that make the circle round
+        profileImageView.layer.cornerRadius = 40 / 2
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
