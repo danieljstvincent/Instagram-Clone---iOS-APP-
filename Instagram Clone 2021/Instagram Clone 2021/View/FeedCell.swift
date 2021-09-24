f44f3a1dd66ab16e81b59a7edcd1276188fb79b7
Daniel <dannystvincent@gmail.com>
2021-09-24 15:48:37 -0400

    Name displaying next to image

--- Instagram Clone 2021/Instagram Clone 2021/View/FeedCell.swift
+++ Instagram Clone 2021/Instagram Clone 2021/View/FeedCell.swift
@@ -18,7 +18,18 @@ class FeedCell: UICollectionViewCell {
         iv.image = #imageLiteral(resourceName: "venom-7")
         return iv
     } ()
+    
+    private lazy var usernameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("venom", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
+        return button
+    } ()
     // MARK: - Lifecycle
+    
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,9 +41,18 @@ class FeedCell: UICollectionViewCell {
         profileImageView.setDimensions(height: 40, width: 40)
 // Below is the line of code that make the circle round
         profileImageView.layer.cornerRadius = 40 / 2
+        
+        addSubview(usernameButton)
+        usernameButton.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor,
+                               paddingLeft: 8)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    // MARK: - Actions
+    
+    @objc func didTapUsername() {
+        print("DEBUG: did tap username")
     }
 }
