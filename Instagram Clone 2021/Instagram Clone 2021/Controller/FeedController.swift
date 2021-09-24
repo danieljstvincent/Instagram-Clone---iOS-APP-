778af79db9b527eda2972814fc95be52e994eb09
Daniel <dannystvincent@gmail.com>
2021-09-23 20:20:53 -0400

    I am getting a error and have no idea what it is :( I plan to pull that last working version tomorrow

--- Instagram Clone 2021/Instagram Clone 2021/Controller/FeedController.swift
+++ Instagram Clone 2021/Instagram Clone 2021/Controller/FeedController.swift
@@ -7,11 +7,45 @@
 
 import UIKit
 
-class FeedController: UIViewController {
+private let reuseIdentifier = "Cell"
+
+class FeedController: UICollectionViewController {
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+       
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        collectionView.backgroundColor = .white
         
-        view.backgroundColor = .systemRed
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+// This funation tell the collect view home many cell to create
+extension FeedController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+//
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+}
+
+// Sizing for cell
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionview(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
     }
 }
--- Instagram Clone 2021/Instagram Clone 2021/Controller/MainTabController.swift
+++ Instagram Clone 2021/Instagram Clone 2021/Controller/MainTabController.swift
@@ -20,17 +20,29 @@ class MainTabController: UITabBarController {
     func configureViewControllers() {
         view.backgroundColor = .white
 
-        let feed = FeedController()
+        let layout = UICollectionViewFlowLayout()
+        
+        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
 
-        let search = SearchController()
+        let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
 
-        let imageSelector = ImageSeletorController()
+        let imageSelector = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSeletorController())
 
-        let notifications = NotificaitonController()
+        let notifications = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificaitonController())
 
-        let profile = ProfileController()
+        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController())
 
         viewControllers = [feed, search, imageSelector, notifications, profile ]
+        
+        tabBar.tintColor = .black
+    }
+    
+    func templateNavigationController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        return nav
     }
 }
 
