6086030288d000c00893af79cb347499ea099dc2
Daniel <dannystvincent@gmail.com>
2021-10-01 11:23:01 -0400

    Username and Password inout displaying

--- Instagram Clone 2021/Instagram Clone 2021/Controller/Authentication/LoginController.swift
+++ Instagram Clone 2021/Instagram Clone 2021/Controller/Authentication/LoginController.swift
@@ -11,7 +11,7 @@ class LoginController: UIViewController {
     
     // MARK: - Properties
     private let iconImage: UIImageView = {
-        let iv = UIImageView(image: #imageLiteral(resourceName: <#T##String#>))
+        let iv = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
         iv.contentMode = .scaleAspectFill
         return iv
     }()
@@ -26,6 +26,7 @@ class LoginController: UIViewController {
         tf.backgroundColor = UIColor(white: 1, alpha: 0.1)
         tf.setHeight(50)
         tf.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [.foregroundColor:UIColor(white: 1, alpha: 0.7)])
+        tf.isSecureTextEntry = true
         return tf
     }()
     
@@ -38,9 +39,34 @@ class LoginController: UIViewController {
         tf.keyboardType = .emailAddress
         tf.backgroundColor = UIColor(white: 1, alpha: 0.1)
         tf.setHeight(50)
-        tf.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [.foregroundColor:UIColor(white: 1, alpha: 0.7)])
+        tf.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor:UIColor(white: 1, alpha: 0.7)])
         return tf
     }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.setHeight(50)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+    
+    private let dontHaveAccountButton : UIButton = {
+        let button = UIButton(type: .system)
+        
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font:UIFont.systemFont(ofSize: 16)]
+        
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ", attributes: atts)
+        
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: boldAtts))
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    } ()
   // MARK: - LifeCycle
         override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,5 +92,18 @@ class LoginController: UIViewController {
         iconImage.centerX(inView: view)
         iconImage.setDimensions(height: 80, width: 120)
         iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton, dontHaveAccountButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+//        view.addSubview((dontHaveAccountButton)
+//        dontHaveAccountButton.centerX(inView: view)
+//        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
+        
     }
 }
